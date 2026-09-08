0b4f40178c77bc52c8eec14737b74b7e75366706
adamsvaty666 <adamsvaty666@gmail.com>
2026-09-08 16:18:49 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+function sayYes() {
+
+    document.getElementById("main").style.display = "none";
+
+    document.getElementById("noResult").style.display = "none";
+
+    document.getElementById("yesResult").style.display = "block";
+
+}
+
+
+function sayNo() {
+
+    document.getElementById("main").style.display = "none";
+
+    document.getElementById("yesResult").style.display = "none";
+
+    document.getElementById("noResult").style.display = "block";
+
+}
+
+
+function help() {
+
+    alert(
+        "nigga what"
+    );
+
+}
+
